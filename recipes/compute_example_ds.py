f70f5b26920d8990efd7e68cf2ca9892a0879722
admin <admin>
2022-07-14 15:07:08 +1000

    Saved recipe: VERSION_CONTROL.compute_example_ds

---
+++ recipes/compute_example_ds.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+import dataiku
+import pandas as pd, numpy as np
+from dataiku import pandasutils as pdu
+
+
+
+# Compute recipe outputs
+# TODO: Write here your actual code that computes the outputs
+# NB: DSS supports several kinds of APIs for reading and writing data. Please see doc.
+
+example_ds_df = pd.DataFrame(data = pd.date_range(start = '2022-01-25', end = '2022-05-01'))
+
+
+# Write recipe outputs
+example_ds = dataiku.Dataset("example_ds")
+example_ds.write_with_schema(example_ds_df)
