24c73050317425d6a2302dc9fd563a26f453a15b
admin <admin>
2022-09-15 13:48:21 +1000

    Duplicated project VERSION_CONTROL into VERSION_CONTROL_ANOTHER_BRANCH

---
+++ lib/python/utils_func.py
@@ -0,0 +1,11 @@
+# some example utilities type functions to be included here; some data wrangling examples
+
+# test creating a function
+def test():
+  print("hello world")
+  
+def add(x):
+  print(x + 2)
+
+def subtract(x):
+  print(x - 2)
